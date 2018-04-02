695f83ae8a78f8ff1bd063e6547398940468c24d
viniciusofp <viniciusofp@gmail.com>
2018-04-02 12:36:59 -0300

    signup mailchimp configuration

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,20 +1,37 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class SignupForm extends React.Component {
+  state = {
+    success: false,
+  }
+
   render () {
     return (
       <div style={wrapperStyle}>
         <div>
           <p style={textStyle}>Digite seu e-mail e pressione Enter para receber o Boletim informativo FORA diretamente em sua caixa de e-emails.</p>
           <input type='text' style={inputStyle} onKeyDown={this.handleKeyDown} />
+          {this.state.success && <p style={textStyle}>Seu email foi adicionado com sucesso.</p>}
         </div>
       </div>
     )
   }
 
   handleKeyDown = (e) => {
+    const self = this;
     if (e.keyCode !== 13) return
-    console.log('enter')
+    axios.post('/signup/' + e.target.value, {
+    })
+    .then(function (response) {
+      self.setState({
+        success: true
+      })
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 }
 
